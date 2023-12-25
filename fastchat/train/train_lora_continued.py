2b837be658194b15d6b59c4492c39cde57cba5a6
xxchenxx <hfercxx@hotmail.com>
2023-12-25 17:17:29 +0000

    update

--- fastchat/train/train_lora_continued.py
+++ fastchat/train/train_lora_continued.py
@@ -100,6 +100,7 @@ class LlamaForCausalLM(transformers.models.llama.modeling_llama.LlamaForCausalLM
             print(shift_logits.shape)
             print(shift_labels.shape)
             loss = loss_fct(shift_logits, shift_labels)
+            print(loss.shape)
             print(loss.mean())
             loss = loss.mean()
             # loss = loss.reshape(bs, -1)
